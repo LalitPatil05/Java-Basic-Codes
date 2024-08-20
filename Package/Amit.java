6451bdb6772dee01d7f3d855a847249f3a4ed912
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-20 22:30:30 +0530

    [ADDED] : Lalit - Demonstration of Creating and Using Package in Java.

---
+++ Package/Amit.java
@@ -0,0 +1,19 @@
+package Package1;
+
+public class Amit
+{
+	private int x;
+	private int y;
+
+	public void SetData(int a, int b){
+	x=a;
+	y=b;;
+}
+
+	public void Display(){
+	System.out.println("Hello Package1");
+	System.out.println(x);
+	System.out.println(y);
+
+}
+}
