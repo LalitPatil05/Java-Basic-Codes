dfc5c8325cb609f70e9fa76a582a1273b2f2335b
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-21 23:08:05 +0530

    [ADDED] : Lalit - Demonstration of Double Class in Java.

---
+++ WrapperClassDouble.java
@@ -0,0 +1,9 @@
+class WrapperClassDouble{
+    public static void main(String args[]){
+        Double D1 = Double.valueOf("55");
+
+        double Result = D1.doubleValue();
+
+        System.out.println(Result);
+    }
+}
