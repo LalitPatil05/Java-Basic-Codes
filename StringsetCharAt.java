c916d27ce9d7d22d80fd74a611529cb1018d0dfc
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-29 11:57:35 +0530

    [ADDED] : Lalit - Demonstration of String setCharAt() Function in Java.

---
+++ StringsetCharAt.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class StringsetCharAt {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("Kolkatta");
+        System.out.println(str.setCharAt(2, 'C'));
+    }
+}
