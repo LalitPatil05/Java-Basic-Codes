0109ca710e292ed081770b118e2db13a1208d043
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-29 11:42:12 +0530

    [ADDED] : Lalit - Demonstration of String CompareTO() Function in Java.

---
+++ StringCompareTo.java
@@ -0,0 +1,8 @@
+public class StringCompareTo {
+    public static void main(String[] args) {
+        String str1 = "A";
+        String str2 = "Z";
+
+        System.out.println(str1.compareTo(str2));
+    }
+}
