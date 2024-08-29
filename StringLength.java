00908147efaf87606ef03cccda6e03525352049f
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-29 11:51:23 +0530

    [ADDED] : Lalit - Demonstration of Strig Length() Function in Java.

---
+++ StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "HelloJavaProgramming";
+        System.out.println("String Length is : "+str.length());
+    }
+}
