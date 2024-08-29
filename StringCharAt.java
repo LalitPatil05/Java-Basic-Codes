80fae3d0e93712a8d2744379e2eaf333d00889cc
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-29 11:39:29 +0530

    [ADDED] : Lalit - Demonstration of String charAt() Function in java.

---
+++ StringCharAt.java
@@ -0,0 +1,7 @@
+public class StringCharAt {
+    public static void main(String[] args) {
+        String str = "Kolkatta";
+        System.out.println(str.charAt(0));
+        //System.out.println(str.charAt(0,"C"));
+    }
+}
