8ffb860c9ba01829ecd509a992d63a05ac803f7a
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-29 11:58:54 +0530

    [ADDED] : Lalit - Demonstration of String substring() Function in Java.

---
+++ StringSubString.java
@@ -0,0 +1,6 @@
+public class StringSubString {
+    public static void main(String[] args) {
+        String str = "HelloJavaProgramming";
+        System.out.println(str.substring(0,20));
+    }
+}
