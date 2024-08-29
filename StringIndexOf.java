2665d1730a66ef228c8b188e4da4754dbb7a202d
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-29 11:49:53 +0530

    [ADDED] : Lalit - Demonstration of String IndexOf() Function in Java.

---
+++ StringIndexOf.java
@@ -0,0 +1,8 @@
+public class StringIndexOf {
+    public static void main(String[] args) {
+        String str = "HelloJavaProgramming";
+        System.out.println(str.indexOf("Java"));
+        System.out.println(str.indexOf("P", 0));
+        System.out.println(str);
+    }
+}
