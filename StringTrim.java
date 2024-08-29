26f064e944b05bcf58b1b704b1e06e66d7f7a415
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-29 12:01:37 +0530

    [ADDED] : Lalit - Demonstration of String Trim() Function in Java.

---
+++ StringTrim.java
@@ -0,0 +1,8 @@
+public class StringTrim {
+    public static void main(String[] args) {
+        String str1 = " Hiii ";
+        String str2 = "Java";
+
+        System.out.println(str1.trim() + str2);
+    }
+}
