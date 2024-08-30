dcc2a85c8590c819b3d80cdee2ea72f03fc34b3a
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-30 20:48:19 +0530

    [ADDED] : Lalit - Demonstration of InstanceOf Operator in Java.

---
+++ InstanceOfClass.java
@@ -0,0 +1,7 @@
+public class InstanceOfClass {
+    public static void main(String[] args) {
+        String name = "JavaProgramming";
+        boolean result = name instanceof String;
+        System.out.println("Result is : "+result);
+    }
+}
