d011ae2182f9ebb54bc9c66f7a68e67d454140f9
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-31 20:31:03 +0530

    [ADDED] : Lalit - Demonstration of StringBuffer Insert Function in Java

---
+++ StringInsert.java
@@ -0,0 +1,6 @@
+public class StringInsert {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("JavaProgramming");
+        System.out.println(str.insert(0,"Hello "));
+    }
+}
