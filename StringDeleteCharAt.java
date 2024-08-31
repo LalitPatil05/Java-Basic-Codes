cc0e186a7dfc45ff3568d06d2e616c4623f14e42
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-31 20:25:44 +0530

    [ADDED] : Lalit - Demonstration of StringBuffer DeleteCharAt Function in Java

---
+++ StringDeleteCharAt.java
@@ -0,0 +1,6 @@
+public class StringDeleteCharAt {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("JavaProgramming");
+        System.out.println(str.deleteCharAt(4l));
+    }
+}
