8dcaea82eed30adb6f65a2b79dc6f4136a968b66
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-31 20:35:05 +0530

    [ADDED] : Lalit - Demonstration of StringBuffer SetLength Function in Java

---
+++ StringSetLength.java
@@ -0,0 +1,9 @@
+public class StringSetLength {
+    public static void main(String[] args) {
+
+        StringBuffer str = new StringBuffer("JavaProgramming");
+
+        str.setLength(3);
+
+    }
+}
