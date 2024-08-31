71b63cc1923fa861d902d4ec6b4bbc704a54d2bf
LalitPatil05 <lalitpatil4636@gmail.com>
2024-08-31 20:23:51 +0530

    [ADDED] : Lalit - Demonstration of StringBuffer String Delete Function in Java

---
+++ StringDelete.java
@@ -0,0 +1,8 @@
+public class StringDelete {
+    public static void main(String[] args) {
+        
+         StringBuffer str = new StringBuffer("Maharashtra");
+         System.out.println(str.delete(0, 5));
+
+    }
+}
