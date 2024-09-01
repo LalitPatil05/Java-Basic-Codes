448f76e7a37f263b8ea91af968a6940a0179e670
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-01 21:23:37 +0530

    [MODIFIED] : Lalit - Demonstration of String Getchar() Function in Java

--- StringGetchar.java
+++ StringGetchar.java
@@ -2,6 +2,7 @@ public class StringGetchar {
     public static void main(String[] args) {
         String str = "HelloJava";
         char ch[] = new char[4];
-        System.out.println(str.getChars(0, 4, ch, 0));
+        str.getChars(0, 4, ch, 0);
+        System.out.println(ch);
     }
 }
