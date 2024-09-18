8cb7f1cd7ddeaa62417a46c5dea996a86bec3832
LalitPatil05 <lalitpatil4636@gmail.com>
2024-09-19 00:51:19 +0530

    [ADDED] : Lalit - Demonstration of Class & Object in Java.

---
+++ Practical10.java
@@ -0,0 +1,15 @@
+class DemoClass
+{
+int x = 501;
+void Display(){
+System.out.println("HelloJavaProgramming");
+System.out.println("Value of x is : "+x);
+}
+}
+
+public class Practical10{
+public static void main(String[] args){
+DemoClass DC = new DemoClass();
+DC.Display();
+}
+}
